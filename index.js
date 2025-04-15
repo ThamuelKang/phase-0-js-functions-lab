6f529dea0e0879cccc518b5fedd0ad78955aed17
Samuel Kang <samuel.june.kang@gmail.com>
2025-04-14 19:31:22 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+function calculateTax(amount) {
+    return amount * .1
+}
 
+function convertToUpperCase(text) {
+    return text.convertToUpperCase();
+}
 
+function findMaximum(num1,num2) {
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
 
+function isPalindrome(word) {
+    const reverse = word.split('').reverse().join('')
+    return word === reverse
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const convertPercentage = discountPercentage * .01
+
+    return originalPrice * convertPercentage
+}
 
 
 // This is required for the test to function properly  
